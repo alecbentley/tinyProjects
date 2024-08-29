d9449f60bbcaae837b9fec7f9945aa67aabb7791
alecbentley <alecsgit@nets.slmail.me>
2024-08-29 13:33:03 -0700

    added copy and paste

--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -6,15 +6,16 @@ const charset = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 const inputField = document.querySelector("#password-length");
 let parsedInputField;
 
+/**
+ * 	An event listener function to ensure the user cannot input values outside the allowed range.
+ */
 inputField.addEventListener("input", function () {
 	let value = parseInt(this.value);
-	if (value < 14) {
-		this.value = 14;
-	} else if (value > 19) {
-		this.value = 19;
+	if (value < 13 || value > 19) {
+		this.value = parsedInputField;
+	} else {
+		parsedInputField = value;
 	}
-	this.value = value;
-	parsedInputField = value;
 });
 
 const passwordField1 = document.querySelector(".passwordField1");
@@ -22,6 +23,9 @@ const passwordField2 = document.querySelector(".passwordField2");
 passwordField1.textContent = `password 1`;
 passwordField2.textContent = `password 2`;
 
+/**
+ * Generates the password using predefined length using characters from the charset variable.
+ */
 function generate(length) {
 	let password = "";
 	for (let i = 0; i < length; i++) {
@@ -31,6 +35,10 @@ function generate(length) {
 	return password;
 }
 
+/**
+ * Populates the two password fields with randomly generated strings
+ * of the same length, given by the value of the input field.
+ */
 function fillPasswords() {
 	const password1 = generate(parsedInputField);
 	const password2 = generate(parsedInputField);
@@ -38,4 +46,21 @@ function fillPasswords() {
 	passwordField2.textContent = password2;
 	console.log(`password 1 length is ${password1.length} chars`);
 	console.log(`password 2 length is ${password2.length} chars`);
+}
+
+/**
+ *	Copies the password to the clipboard
+ */
+function copy() {
+	const passwordField =
+		document.querySelector(".passwordField1").textContent ===
+		document.activeElement.textContent
+			? document.querySelector(".passwordField1")
+			: document.querySelector(".passwordField2");
+	navigator.clipboard.writeText(passwordField.textContent);
+	console.log(
+		`copied password ${
+			passwordField.textContent === passwordField1.textContent ? 1 : 2
+		}`
+	);
 }
