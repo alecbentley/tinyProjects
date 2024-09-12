f0be586331c40001346815d204f6467cf39a21ee
alecbentley <alecsgit@nets.slmail.me>
2024-09-11 17:10:17 -0700

    added 'Enter' key input

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -4,12 +4,13 @@
  **                            fetch DOM
  *========================================================================**/
 
-let messageEl = document.querySelector(".message").textContent;
-let scoreEL = document.querySelector(".score").textContent;
-let bigNumberEl = document.querySelector(".number").textContent;
+const messageEl = document.querySelector(".message");
+const scoreEL = document.querySelector(".score");
+const bigNumberEl = document.querySelector(".number");
 const highscoreEl = document.querySelector(".highscore");
 const inputField = document.querySelector(".guess");
 const checkButton = document.querySelector(".check");
+const pageBody = document.querySelector("body");
 
 /**========================================================================
  **                       Generate random number 1-20
@@ -25,36 +26,62 @@ console.log(randomNumber);
 let score = 20;
 
 /**========================================================================
- *?                            checkButton eventListener
+ *TODO                            Button logic
  *========================================================================**/
 
-checkButton.addEventListener("click", function () {
-	const guess = parseInt(inputField.value);
+//eventListener not added here because it needs to be 'click' & 'Enter'. Scroll further down to see the eventListener
+function handleInput() {
+	const guess = parseInt(inputField.value); //convert input field string to number
+
+	//check to make sure user has input a value
 	if (!guess) {
-		messageEl = `please input a number`;
+		messageEl.textContent = `please input a number`;
+
+		//otherwise, if user guesses correct
 	} else if (guess === randomNumber) {
-		messageEl = `You guessed correct!!!`;
-		bigNumberEl = randomNumber;
+		pageBody.style.backgroundColor = "#60b347";
+		messageEl.textContent = `You guessed correct!!!`;
+		bigNumberEl.textContent = randomNumber;
 		inputField.value = "";
+
+		//otherwise, if user guesses too high
 	} else if (guess > randomNumber) {
 		if (score > 1) {
-			messageEl = `You guessed too high`;
+			//make sure score is above 0 first, then activate code
+			messageEl.textContent = `You guessed too high`;
 			score--;
-			scoreEL = `${score}`;
+			scoreEL.textContent = `${score}`;
 			inputField.value = "";
 		} else {
-			scoreEL = `0`;
-			messageEl = `You loose!`;
+			//if user loses do this
+			scoreEL.textContent = `0`;
+			messageEl.textContent = `You loose!`;
 		}
+
+		//otherwise, if user guesses too low
 	} else if (guess < randomNumber) {
 		if (score > 1) {
-			messageEl = `You guessed too low`;
+			//make sure score is above 0 first, then activate code
+			messageEl.textContent = `You guessed too low`;
 			score--;
-			scoreEL = `${score}`;
+			scoreEL.textContent = `${score}`;
 			inputField.value = "";
 		} else {
-			scoreEL = `0`;
-			messageEl = `You loose!`;
+			//if user loses do this
+			scoreEL.textContent = `0`;
+			messageEl.textContent = `You loose!`;
 		}
+	}
+}
+
+/**========================================================================
+ **                            "Check" button eventListener
+ *========================================================================**/
+
+checkButton.addEventListener("click", handleInput);
+
+document.addEventListener("keydown", function (event) {
+	if (event.key === "Enter") {
+		handleInput();
 	}
 });
