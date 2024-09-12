712c707e56d2ae9b284047e98e8a53bc4ef64287
alecbentley <alecsgit@nets.slmail.me>
2024-09-11 18:10:38 -0700

    added full 'again' button functionality

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -11,25 +11,34 @@ const highscoreEl = document.querySelector(".highscore");
 const inputField = document.querySelector(".guess");
 const checkButton = document.querySelector(".check");
 const pageBody = document.querySelector("body");
+const againButton = document.querySelector(".again");
 
 /**========================================================================
  **                       Generate random number 1-20
  *========================================================================**/
 
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(randomNumber);
+let randomNumber;
+function randoNum() {
+	randomNumber = Math.trunc(Math.random() * 20) + 1;
+}
+randoNum();
+
+console.log(`secret number is ${randomNumber}`);
 
 /**========================================================================
- **                            declare state variable
+ **                            declare state variables
  *========================================================================**/
 
 let score = 20;
 
+let finalScoresArray = []; //for collecting scores
+let max = [];
+
 /**========================================================================
- *TODO                            Button logic
+ *TODO                         "Check" Button logic
  *========================================================================**/
 
-//eventListener not added here because it needs to be 'click' & 'Enter'. Scroll further down to see the eventListener
+//eventListener not added here because it needs to be 'click' & 'Enter'. Scroll further down to see the eventListener. This code goes into eventListener.
 function handleInput() {
 	const guess = parseInt(inputField.value); //convert input field string to number
 
@@ -42,6 +51,8 @@ function handleInput() {
 		pageBody.style.backgroundColor = "#60b347";
 		messageEl.textContent = `You guessed correct!!!`;
 		bigNumberEl.textContent = randomNumber;
+		finalScoresArray.push(score);
+		console.log(`FINAL SCORES ARE: ${finalScoresArray}`);
 		inputField.value = "";
 
 		//otherwise, if user guesses too high
@@ -75,7 +86,7 @@ function handleInput() {
 }
 
 /**========================================================================
- **                            "Check" button eventListener
+ *?                            "Check" button eventListeners
  *========================================================================**/
 
 checkButton.addEventListener("click", handleInput);
@@ -84,4 +95,32 @@ document.addEventListener("keydown", function (event) {
 	if (event.key === "Enter") {
 		handleInput();
 	}
+});
+
+/**========================================================================
+ *?                            "Again" button eventListener
+ *========================================================================**/
+
+againButton.addEventListener("click", function () {
+	console.log(`new game ====`);
+	messageEl.textContent = `Let's play again. Guess a number!`;
+	bigNumberEl.textContent = `?`;
+
+	for (let i = 0; i < finalScoresArray.length; i++) {
+		if (finalScoresArray[i] > max) {
+			max = finalScoresArray[i];
+		}
+	}
+	console.log(`forloop determines highest score as ${max}`);
+
+	highscoreEl.textContent = `${max}`;
+	console.log(`max = ${max}`);
+	score = 20;
+	scoreEL.textContent = `${score}`;
+
+	randoNum();
+
+	pageBody.style.backgroundColor = "#222";
+
+	console.log(`new random number ${randomNumber}`);
 });
